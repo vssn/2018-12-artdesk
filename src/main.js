9dbad022d2d4dd2e4446bde27827f83769deea3e
vssn <matrim.eg@gmail.com>
2018-12-27 21:20:53 +0100

    removed unnecessary comments and objects

--- src/main.js
+++ src/main.js
@@ -11,15 +11,6 @@ import Skills from './components/Skills.vue';
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
-// 1. Define route components.
-// These can be imported from other files
-const Bar = { template: '<div>bar</div>' };
-
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', component: HelloWorld },
@@ -28,11 +19,8 @@ const routes = [
   { path: '/contact', component: Contact },
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
-  routes, // short for `routes: routes`
+  routes,
 });
 
 /* eslint-disable no-new */
